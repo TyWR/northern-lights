adbd79543a83d3bcd7fb3f9fcaa8041266616383
Savagerqt <tanguy.viv@gmail.com>
2019-12-06 13:03:50 +0100

    Adapt page to full screen

--- static/index.js
+++ static/index.js
@@ -1,7 +1,33 @@
-var svg = d3.select("#world")
+var svg = d3.select("#world");
+var width = parseInt(svg.style("width"));
+var height = parseInt(svg.style("height"));
+
+var x = d3.scaleLinear()
+  .range([ 0, width ])
+  .domain([ -180, 180 ]);
+
+var y = d3.scaleLinear()
+    .range([ 0, height ])
+    .domain([ -90, 90 ]);
+
+var color = d3.scaleLinear()
+    .range( ['#a3be8c', '#a3be8c', '#b48ead'] )
+    .domain( [0, 100] );
+
+var alpha = d3.scaleLinear()
+    .range( [0, 1, 1] )
+    .domain( [0, 100] );
 
 d3.json("/data", function(data) {
     console.log(data);
     svg.selectAll()
+	   .data(data.points)
+	   .enter()
 	   .append("rect")
+	   .attr("x", function(d) { return x(d.longitude) })
+	   .attr("y", function(d) { return y(d.latitude) })
+	   .attr("width", "7")
+	   .attr("height", "7")
+	   .attr("fill", function(d) { return color(d.power) })
+	   .attr("fill-opacity", function(d) { return alpha(d.power) });
 })
